74c5aa3bc41c5afbcfbced05ebc3006973294f03
mattisalan <matti.salan@gmail.com>
2023-06-13 10:19:12 +0300

    Create one Block at game start

--- data/gameobjects/player.py
+++ data/gameobjects/player.py
@@ -1,9 +1,13 @@
 import pygame
 
 from data.settings import COLORS
+from data.settings import GFX
 
+IMAGES = [pygame.image.load(image).convert_alpha() for image in GFX]
+SPEED_Y = 100
+SPEED_X = 20
 
-class Player(pygame.sprite.Sprite):
+class Block(pygame.sprite.Sprite):
 
     def __init__(self, screen_rect):
         super().__init__()
@@ -11,4 +15,23 @@ class Player(pygame.sprite.Sprite):
 
         self.image = pygame.surface.Surface((40, 40))
         self.image.fill(COLORS["RED"])
-        self.rect = self.image.get_rect(center=self.screen_rect.center)
+        self.rect = self.image.get_rect(centerx=self.screen_rect.centerx, y=0)
+
+        # Floating point position
+        self.true_y = self.rect.y
+
+        self.speed_x = 0
+
+    def move_x(self, dir=1):
+        self.speed_x = dir * SPEED_X
+
+    def move(self, dt):
+        self.true_y += SPEED_Y * dt
+
+        self.rect.x += self.speed_x
+        self.rect.y = round(self.true_y)
+
+        self.speed_x = 0
+
+    def update(self, dt):
+        self.move(dt)
--- data/states/gameplay.py
+++ data/states/gameplay.py
@@ -2,7 +2,7 @@ import pygame
 
 from .state import State
 from data.settings import COLORS
-from data.gameobjects.player import Player
+from data.gameobjects.player import Block
 
 
 class Gameplay(State):
@@ -11,21 +11,23 @@ class Gameplay(State):
         super().__init__()
         self.next_state = "GAMEOVER"
 
-        self.player = Player(self.screen_rect)
-        self.player_group = pygame.sprite.GroupSingle(self.player)
+        self.current_block = Block(self.screen_rect)
+        self.current_block_group = pygame.sprite.GroupSingle(self.current_block)
 
     def handle_events(self, events):
         """Event handler"""
         for event in events:
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_SPACE:
-                    self.done = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.current_block.move_x()
+                elif event.key == pygame.K_LEFT:
+                    self.current_block.move_x(-1)
     
     def update(self, dt):
         """Game logic"""
-        pass
+        self.current_block_group.update(dt)
     
     def draw(self, screen):
         """Renders the graphics"""
         screen.fill(COLORS["BLACK"])
-        self.player_group.draw(screen)
+        self.current_block_group.draw(screen)
